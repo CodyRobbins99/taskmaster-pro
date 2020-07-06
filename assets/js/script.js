100c65cc4baa4e1faa2f2ffeee59cf5595f5d45e
Coodie <65614373+CodyRobbins99@users.noreply.github.com>
2020-07-06 15:36:01 -0400

    webpage styling touch ups

--- assets/js/script.js
+++ assets/js/script.js
@@ -161,7 +161,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -197,6 +197,22 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
+  activate: function(event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  over: function(event) {
+    $(event.target).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
+  },
+  out: function(event) {
+    $(event.target).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active");
+  },
   update: function(event) {
     // array to store the task data in
     var tempArr = [];
@@ -221,6 +237,7 @@ $(".card .list-group").sortable({
         date: date
       });
     });
+    $(".bottom-trash").removeClass("bottom-trash-active");
     // trim down list's ID to match object property
     var arrName = $(this)
       .attr("id")
@@ -269,3 +286,9 @@ var auditTask = function(taskEl) {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
